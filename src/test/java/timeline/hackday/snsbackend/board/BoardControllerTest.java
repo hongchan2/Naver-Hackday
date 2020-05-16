ba5012f29d121c7ef9c91bc9b584758139af5829
hongchan2 <ipeter@naver.com>
2020-05-17 00:04:59 +0900

    test: test code for create board API

---
+++ src/test/java/timeline/hackday/snsbackend/board/BoardControllerTest.java
@@ -0,0 +1,65 @@
+package timeline.hackday.snsbackend.board;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+import timeline.hackday.snsbackend.account.Account;
+import timeline.hackday.snsbackend.common.BaseControllerTest;
+import timeline.hackday.snsbackend.common.TestDescription;
+
+public class BoardControllerTest extends BaseControllerTest {
+
+	@Test
+	@TestDescription("정상적으로 게시물 생성하는 테스트")
+	public void createBoard_is_Ok() throws Exception {
+		Account savedAccount = getAccount();
+
+		Board board = getBoard(savedAccount);
+
+		mockMvc.perform(post("/api/board")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(board)))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("title").value(board.getTitle()))
+			.andExpect(jsonPath("content").value(board.getContent()))
+			.andExpect(jsonPath("account.username").value(board.getAccount().getUsername()));
+	}
+
+	@Test
+	@TestDescription("필수 입력 값을 빼먹어 예외가 발생하는 테스트")
+	public void createBoard_Bad_Request() throws Exception {
+		Account savedAccount = getAccount();
+
+		Board board = getBoard(savedAccount);
+		board.setContent(null);
+
+		mockMvc.perform(post("/api/board")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(board)))
+			.andDo(print())
+			.andExpect(status().isBadRequest());
+	}
+
+	private Board getBoard(Account savedAccount) {
+		return Board.builder()
+			.title("test title")
+			.content("test content")
+			.createTime(LocalDateTime.now())
+			.account(savedAccount)
+			.build();
+	}
+
+	private Account getAccount() {
+		Account account = new Account();
+		account.setUsername("hongchan");
+		account.setPassword("1234");
+		return accountRepository.save(account);
+	}
+}
