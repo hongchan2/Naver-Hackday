955ff9fb958c55b8bf2e1db6d3635ffe408f0954
hongchan2 <ipeter@naver.com>
2020-05-17 20:14:09 +0900

    test: Test code for 'delete follow API'

--- src/test/java/timeline/hackday/snsbackend/follow/FollowControllerTest.java
+++ src/test/java/timeline/hackday/snsbackend/follow/FollowControllerTest.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
 import timeline.hackday.snsbackend.account.Account;
@@ -12,6 +13,9 @@ import timeline.hackday.snsbackend.common.BaseControllerTest;
 import timeline.hackday.snsbackend.common.TestDescription;
 
 public class FollowControllerTest extends BaseControllerTest {
+
+	@Autowired
+	FollowService followService;
 
 	@Test
 	@TestDescription("정상적으로 follow 관계를 생성하는 테스트")
@@ -52,6 +56,48 @@ public class FollowControllerTest extends BaseControllerTest {
 			.content(objectMapper.writeValueAsString(followDto)))
 			.andDo(print())
 			.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	@TestDescription("정상적으로 follow 관계를 삭제하는 테스트")
+	public void deleteFollow_Is_Ok() throws Exception {
+		// Given
+		Account savedAccount1 = getAccountSaved("hongchan", "B31$#23D21$&");
+		Account savedAccount2 = getAccountSaved("jiyun", "B31$#23D21$&");
+
+		FollowDto followDto = new FollowDto();
+		followDto.setSrcId(savedAccount1.getId());
+		followDto.setDestId(savedAccount2.getId());
+
+		followService.follow(followDto);
+
+		// When & Then
+		mockMvc.perform(delete("/api/follow/{srcId}/{destId}", followDto.getSrcId(), followDto.getDestId())
+			.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isOk());
+	}
+
+	@Test
+	@TestDescription("존재하지 않는 follow 관계를 삭제해 예외가 발생하는 테스트")
+	public void deleteFollow_Not_Found() throws Exception {
+		// Given
+		Account savedAccount1 = getAccountSaved("hongchan", "B31$#23D21$&");
+		Account savedAccount2 = getAccountSaved("jiyun", "B31$#23D21$&");
+
+		FollowDto followDto = new FollowDto();
+		followDto.setSrcId(savedAccount1.getId());
+		followDto.setDestId(savedAccount2.getId());
+
+		followService.follow(followDto);
+
+		// When & Then
+		mockMvc.perform(delete("/api/follow/{destId}/{srcId}", followDto.getDestId(), followDto.getSrcId())
+			.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isNotFound());
 	}
 
 }
