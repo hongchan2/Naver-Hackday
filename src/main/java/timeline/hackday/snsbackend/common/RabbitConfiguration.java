0eb5c0cb5e87cb5005958ed789b1d23e379d0d6c
hongchan2 <ipeter@naver.com>
2020-05-20 12:27:18 +0900

    refactor: Move MQ config file to common directory

--- src/main/java/timeline/hackday/snsbackend/common/RabbitConfiguration.java
+++ src/main/java/timeline/hackday/snsbackend/common/RabbitConfiguration.java
@@ -1,4 +1,4 @@
-package timeline.hackday.snsbackend.batch;
+package timeline.hackday.snsbackend.common;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
